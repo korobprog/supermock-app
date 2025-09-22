d0be76b41c0daf030903031a56c6fe5a38d8cff8
korobprog <korobprog@gmail.com>
2025-09-22 10:43:34 +1000

    profil

--- server/prisma/migrations/20250921091858_daily_co_integration/migration.sql
+++ server/prisma/migrations/20250921091858_daily_co_integration/migration.sql
@@ -1,18 +1,6 @@
--- DropIndex
-DROP INDEX "Notification_userId_createdAt_idx";
-
 -- AlterTable
 ALTER TABLE "InterviewMatch" ADD COLUMN     "roomId" TEXT,
 ADD COLUMN     "roomToken" TEXT;
 
 -- AlterTable
 ALTER TABLE "InterviewerAvailability" ADD COLUMN     "language" TEXT DEFAULT '🇺🇸 English';
-
--- AlterTable
-ALTER TABLE "Notification" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "RealtimeSession" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- CreateIndex
-CREATE INDEX "Notification_userId_createdAt_idx" ON "Notification"("userId", "createdAt");
